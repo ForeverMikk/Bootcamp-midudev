16bec6f8a27fe967f448c8cc9b0ed57ea4bfe80e
Michael Molina <michaelmolina679@gmail.com>
2021-02-17 11:38:51 -0600

    Adding the part1 of the React Bootcampby Midudev

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Message from './Message.js';
 
-function App() {
+const Description = () =>{
+  return <p>Hola este es un ejemplo de multiples componentes wowowowow</p>
+}
+const App= () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message message='Hola Mundo'/>
+      <Message message='Perros'/>
+      <Description />
     </div>
   );
 }
