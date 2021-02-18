53b044195a4001a0dbb98ba2d501129093f00cee
Michael Molina <michaelmolina679@gmail.com>
2021-02-17 18:27:20 -0600

    Adding the part3 of the React Bootcamp of midudev

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React from "react";
 
-function App() {
+const notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "CSS is easy",
+    date: "2019-05-30",
+    important: true,
+  },
+  {
+    id: 3,
+    content: "JS is easy",
+    date: "2019-05-30",
+    important: true,
+  },
+];
+
+// const notes = [];
+
+export default function App() {
   return (
-    <h1>hola Mundo</h1>
+    // <div>
+    //   {notes.map(note => {
+    //     return (
+    //       <p>
+    //         <strong>{note.id}</strong>
+    //         {note.content}
+    //       </p>
+    //     );
+    //   })}
+    // </div>
+
+    <div>
+      {notes.map((note) => (
+        <div key={note.id}>
+          <p>{note.content}</p>
+          <small>
+            <time></time>
+          </small>
+        </div>
+      ))}
+    </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,63 +1,8 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-const WarningNotUsed = () => {
-  return <p>Todavia no se ha usado el contador</p>
-}
+const rootElement = document.getElementById("root");
 
-const ListOfClicks = ({clicks}) => {
-  // debugger;
-  return <p>{clicks.join(", ")}</p>
-}
-const App = ({valorInicial}) => {
-  // const [left, setLeft] = useState(0);
-  // const [right, setRight] =useState(0);
-
-  const [counters, setCounters] = useState({
-    right: 0,
-    left: 0, 
-    // clicks: 0,
-    mensaje: "Hola que hace"
-  })
-
-  // Hay que evitar usar Estados Para cualquier cosa
-  const[clicks, setClicks] = useState([])
-
-  const handleLeft = () => {
-    const newCountersState = {
-      ...counters,
-      left: counters.left + 1,
-    };
-    setCounters(newCountersState)
-    setClicks(prevClicks => [...prevClicks,'L'])
-  }
-  
-  const handleRight = () => {
-    setCounters({
-      ...counters,
-      right: counters.right + 1,
-    })
-    setClicks(prevClicks => [...prevClicks,'R'])
-  }
-
-  return (
-    <div>
-      <p>Clicks totales: {clicks.length}</p>
-      <p>{counters.mensaje}</p>
-      {counters.left}
-      <button onClick={handleLeft}>Left</button>
-      <button onClick={handleRight}>Right</button>
-      {counters.right}
-      <p>Clicks totales: {clicks.length}</p>
-      {/* RENDERISADO CONDICIONAL */}
-      {clicks.length === 0
-      ? <WarningNotUsed />
-      : <ListOfClicks clicks={clicks}/>}
-    </div>
-  )
-}
-
-const rootElement = document.getElementById("root");  
-
-ReactDOM.render(<App valorInicial={0}/>, rootElement);
+ReactDOM.render(<App />, rootElement);
