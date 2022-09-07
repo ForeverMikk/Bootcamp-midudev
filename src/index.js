d85c56fa83ec0d4e218c88ff1bd97a155564b947
unknown <michaelmolina679@gmail.com>
2022-09-07 13:26:55 -0500

    El estado del Componente

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Counter from './part1/Counter';
-import Message from './part1/Message';
+// import Message from './part1/Message';
 
 const App = () => {
   
 
   return (
     <div>
-      <Message />
+      {/* <Message /> */}
       <Counter />
     </div>
   )
--- src/part1/Counter.js
+++ src/part1/Counter.js
@@ -6,29 +6,64 @@ const CounterComponent = ({number}) => {
 } 
 
 const Counter = () => {
-    const [contador, setContador] = useState(0);
-    const isEven = contador % 2 === 0;
+    const [counter, setCounter] = useState({
+        left: 0,
+        right: 0,
+        message: "Mensaje del Contador"
+    });
+    
+    const [clicks, setClicks] = useState([]);
+
+    const isEven = counter.clicks % 2 === 0;
     const mensajePar = isEven ? "Es Par" : "Es Inpar";
 
-    const handleClick = () => {
-        setContador(contador + 1)
+    const handleClickLeft = () => {
+        const newCounterState = {
+            ...counter,
+            left: counter.left + 1
+        }
+        setCounter(newCounterState);
+        setClicks(prevClicks => {
+            return [...prevClicks, 'L']
+        })
+    }
+    const handleClickRight = () => {
+        const newCounterState = {
+            ...counter,
+            right: counter.right + 1
+        }
+        setCounter(newCounterState);
+        setClicks(prevClicks => {
+            return [...prevClicks, 'R']
+        });
     }
 
     const handleClickReset = () => {
-        setContador(0)
+        const newCounterState = {
+            ...counter,
+            left: 0,
+            right: 0
+        }
+        setCounter(newCounterState)
     }
 
-    const handleClickRest = () => {
-        setContador(contador - 1)
-    }
+    // const handleClickRest = () => {
+    //     setContador(contador - 1)
+    // }
 
     return (
         <>
-            <CounterComponent number={contador} />
+            <h2>{counter.message}</h2>
+            <CounterComponent number={`Total: ${clicks.length}`} />
+            <CounterComponent number={counter.left} />
+            <CounterComponent number={counter.right} />
             <h2>{mensajePar}</h2>
-            <button onClick={handleClick}>Sumar</button>
+            <button onClick={handleClickLeft}>Izquierda</button>
+            <button onClick={handleClickRight}>Derecha</button>
             <button onClick={handleClickReset}>Reset</button>
-            <button onClick={handleClickRest}>Restar</button>
+
+            {clicks.join(", ")}
+            {/* <button onClick={handleClickRest}>Restar</button> */}
         </>
     )
 }
